ecc47ee29774a30b0a306431a6ea518a5a780c9f
cph560 <74350704+cph560@users.noreply.github.com>
2023-02-08 15:23:43 -0500

    Update utils.py

--- Doc_scanner/utils.py
+++ Doc_scanner/utils.py
@@ -104,5 +104,4 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     else:
         r = width / float(w)
         dim = (width, int(h * r))
-    
     return cv2.resize(image, dim, interpolation=inter)
